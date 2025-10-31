4bae32022bab222de5cbe9a23fdfed9264d5c929
Santiago Luque <luquevillanuevasantiago@gmail.com>
2025-10-31 13:10:27 +0100

    Done

--- src/App.java
+++ src/App.java
@@ -1,5 +1,21 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        int altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura: "));
+
+        for (int i = 0; i <= altura; i++) {
+            for (int j = altura; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (int j = 0; j <= 2 * i; j++) {
+                if (i == altura) {
+                    System.out.print("*");
+                } else if (j == 0 || j == 2 * i) {
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
     }
 }
