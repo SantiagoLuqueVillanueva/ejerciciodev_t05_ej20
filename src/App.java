282fd7c0086fc083970e176e64567548bfb17140
Santiago Luque <luquevillanuevasantiago@gmail.com>
2025-10-31 13:17:09 +0100

    Done2

--- src/App.java
+++ src/App.java
@@ -2,12 +2,12 @@ public class App {
     public static void main(String[] args) throws Exception {
         int altura = Integer.parseInt(System.console().readLine("Por favor, introduzca la altura: "));
 
-        for (int i = 0; i <= altura; i++) {
-            for (int j = altura; j > i; j--) {
+        for (int i = 1; i <= altura ; i++) {
+            for (int j = altura; j >= i; j--) {
                 System.out.print(" ");
             }
-            for (int j = 0; j <= 2 * i; j++) {
-                if (i == altura) {
+            for (int j = 1; j <= 2 * i; j++) {
+                if (i == altura ) {
                     System.out.print("*");
                 } else if (j == 0 || j == 2 * i) {
                     System.out.print("*");
